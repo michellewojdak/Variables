6e333e69ddc49dd5f1725b099fe5fdb3f1a3266f
miwoj2051 <miwoj2051@7L0N6Y1.Student.UGDSB.ED>
2017-03-01 13:59:08 -0500

    variables assignment

---
+++ src/variables/Variables.java
@@ -0,0 +1,57 @@
+/*
+ March 1st 2017
+Variables Assignment
+Introducing the string variable, running last name on print screen
+ */
+
+package variables;
+
+/**
+ *
+ * @author miwoj2051
+ */
+public class Variables  {
+    /**
+     * @param args the command line arguments
+     */
+    
+    
+    
+    
+    double insuranceDeduction = 2.00;
+    double hoursWorked = 40;
+    double tax = 22;
+    double carpetWidth = 8.5;
+    double carpetLength = 6;
+    double carpetPrice = 19.95;
+    double amountPurchased = 12.49;
+    double amountPaid = 20.00;
+    double taxes = 1.13;
+    
+    
+    public static void main(String[] args) {
+        //Area of a Circle
+        double radius = 15;
+        double pi = 3.14;
+        double area = (pi*radius*radius); 
+        System.out.println("The area of the circle is " + area);
+        
+        //Wayne Gretzky
+        String hockeyPlayer = "Wayne Gretzky";
+        int hockeyNumber = 99;
+        System.out.println(hockeyPlayer+" is number "+hockeyNumber);
+        
+        //Area of a Rectangle
+        double length = 4.8;
+        double width = 5.7 ;
+        area = length*width;
+        System.out.println("Length is "+length+"m");
+        System.out.println("Width is "+width+"m");
+        System.out.println("Area is "+area+"m^2");
+        
+        //NetPay
+        
+        // TODO code application logic here
+    }
+    
+}
